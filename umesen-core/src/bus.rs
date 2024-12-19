da82b8f1fe6ece626fbb6794ae0f0b30581b0b8a
Calbabreaker <calbabreaker@gmail.com>
2024-12-19 22:48:18 +1030

    feat: more addressing modes

--- umesen-core/src/bus.rs
+++ umesen-core/src/bus.rs
@@ -1,32 +1,46 @@
+use std::num::NonZeroU32;
+
 pub struct Bus {
-    ram: [u8; 0xffff],
+    pub ram: [u8; 0xffff],
+    /// Cpu cycles counter for debugging
+    pub cpu_cycles: u32,
 }
 
 impl Default for Bus {
     fn default() -> Self {
-        Self { ram: [0; 0xffff] }
+        Self {
+            ram: [0; 0xffff],
+            cpu_cycles: 0,
+        }
     }
 }
 
 impl Bus {
-    pub fn read_byte(&self, address: u16) -> u8 {
+    pub fn read_byte(&mut self, address: u16) -> u8 {
+        self.clock_cpu();
         self.ram[address as usize]
     }
 
     pub fn write_byte(&mut self, address: u16, value: u8) {
+        self.clock_cpu();
         self.ram[address as usize] = value;
     }
 
     pub fn read_word(&mut self, address: u16) -> u16 {
-        let lsb = self.ram[address as usize] as u16;
-        let msb = self.ram[address as usize + 1] as u16;
+        let lsb = self.read_byte(address) as u16;
+        let msb = self.read_byte(address + 1) as u16;
         msb << 8 | lsb
     }
 
     pub fn write_word(&mut self, address: u16, value: u16) {
         let lsb = value as u8;
         let msb = (value << 8) as u8;
-        self.ram[address as usize] = lsb;
-        self.ram[address as usize + 1] = msb;
+        self.write_byte(address, lsb);
+        self.write_byte(address + 1, msb);
+    }
+
+    pub fn clock_cpu(&mut self) {
+        // Every cpu clock is 12 master clocks
+        self.cpu_cycles += 1;
     }
 }
--- umesen-core/src/cpu.rs
+++ umesen-core/src/cpu.rs
@@ -14,10 +14,14 @@ bitflags::bitflags! {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum AddressingMode {
     Accumulator,
+    /// The next byte contains the value
     Immediate,
+    /// The next byte contains the address to the value in the first page (256 bytes)
     ZeroPage,
+    /// Same as ZeroPage + x register
     ZeroPageX,
     ZeroPageY,
     Relative,
@@ -26,7 +30,6 @@ enum AddressingMode {
     AbsoluteY,
     IndirectX,
     IndirectY,
-    IndirectIndexed,
 }
 
 #[derive(Default)]
@@ -46,31 +49,55 @@ pub struct Cpu {
 }
 
 impl Cpu {
+    pub fn execute_next(&mut self) {
+        let opcode = self.read_at_pc();
+        self.execute(opcode);
+    }
+
+    fn read_at_pc(&mut self) -> u8 {
+        let pc = self.pc;
+        self.pc += 1;
+        self.bus.read_byte(pc)
+    }
+
+    fn read_word_at_pc(&mut self) -> u16 {
+        let pc = self.pc;
+        self.pc += 2;
+        self.bus.read_word(pc)
+    }
+
     fn read_address(&mut self, mode: AddressingMode) -> u8 {
         match mode {
             AddressingMode::Accumulator => todo!(),
-            AddressingMode::Immediate => {
-                let value = self.bus.read_byte(self.pc);
-                self.pc += 1;
-                value
+            AddressingMode::Immediate => self.read_at_pc(),
+            AddressingMode::ZeroPage => {
+                let address = self.read_at_pc();
+                self.bus.read_byte(address as u16)
             }
-            AddressingMode::ZeroPage => todo!(),
-            AddressingMode::ZeroPageX => todo!(),
+            AddressingMode::ZeroPageX => {
+                let address = self.read_at_pc().wrapping_add(self.x);
+                self.bus.clock_cpu();
+                self.bus.read_byte(address as u16)
+            }
             AddressingMode::ZeroPageY => todo!(),
             AddressingMode::Relative => todo!(),
-            AddressingMode::Absolute => todo!(),
-            AddressingMode::AbsoluteX => todo!(),
+            AddressingMode::Absolute => {
+                let address = self.read_word_at_pc();
+                self.bus.read_byte(address)
+            }
+            AddressingMode::AbsoluteX => {
+                let base_address = self.read_word_at_pc();
+                let address_x = base_address.wrapping_add(self.x as u16);
+                // Check if page is crossed
+                if base_address >> 8 != address_x >> 8 {
+                    self.bus.clock_cpu();
+                }
+                self.bus.read_byte(address_x)
+            }
             AddressingMode::AbsoluteY => todo!(),
             AddressingMode::IndirectX => todo!(),
             AddressingMode::IndirectY => todo!(),
-            AddressingMode::IndirectIndexed => todo!(),
         }
-    }
-
-    pub fn execute_next(&mut self) {
-        let opcode = self.bus.read_byte(self.pc);
-        self.pc += 1;
-        self.execute(opcode);
     }
 
     fn execute(&mut self, opcode: u8) {
@@ -83,14 +110,25 @@ impl Cpu {
             0xb9 => self.lda(AddressingMode::AbsoluteY),
             0xa1 => self.lda(AddressingMode::IndirectX),
             0xb1 => self.lda(AddressingMode::IndirectY),
+
+            0xa2 => self.ldx(AddressingMode::Immediate),
             _ => panic!("Unknown instruction with opcode: {opcode}"),
         }
     }
 
+    fn set_zero_neg_flags(&mut self, x: u8) {
+        self.flags.set(Flags::ZERO, x == 0);
+        self.flags.set(Flags::NEGATIVE, x & 0b1000000 != 0);
+    }
+
     fn lda(&mut self, mode: AddressingMode) {
         self.a = self.read_address(mode);
-        self.flags.set(Flags::ZERO, self.a == 0);
-        self.flags.set(Flags::NEGATIVE, self.a & 0b1000000 != 0);
+        self.set_zero_neg_flags(self.a);
+    }
+
+    fn ldx(&mut self, mode: AddressingMode) {
+        self.x = self.read_address(mode);
+        self.set_zero_neg_flags(self.x);
     }
 }
 
@@ -99,9 +137,13 @@ mod test {
     use crate::cpu::{Cpu, Flags};
 
     fn execute(rom: &[u8], assert_fn: impl Fn(Cpu)) {
-        let mut cpu = Cpu::default();
-        for (i, byte) in rom.iter().enumerate() {
-            cpu.bus.write_byte(i as u16, *byte);
+        let mut cpu = Cpu {
+            x: 0xff,
+            ..Default::default()
+        };
+        cpu.bus.ram[0x132] = 69;
+        for (i, x) in rom.iter().enumerate() {
+            cpu.bus.ram[i] = *x;
         }
         cpu.execute_next();
         assert_fn(cpu);
@@ -109,8 +151,46 @@ mod test {
 
     #[test]
     fn lda() {
-        execute(&[0xa9, 123], |cpu| assert_eq!(cpu.a, 123));
+        // Immediate mode
+        execute(&[0xa9, 123], |cpu| {
+            assert_eq!(cpu.a, 123);
+            assert_eq!(cpu.bus.cpu_cycles, 2);
+            assert_eq!(cpu.pc, 2);
+        });
         execute(&[0xa9, 0], |cpu| assert_eq!(cpu.flags, Flags::ZERO));
         execute(&[0xa9, 255], |cpu| assert_eq!(cpu.flags, Flags::NEGATIVE));
+
+        // Zero page mode
+        execute(&[0xa5, 0x02, 69], |cpu| {
+            assert_eq!(cpu.a, 69);
+            assert_eq!(cpu.bus.cpu_cycles, 3);
+            assert_eq!(cpu.pc, 2);
+        });
+
+        // Zero page + x mode
+        execute(&[0xb5, 0x03, 69], |cpu| {
+            assert_eq!(cpu.a, 69);
+            assert_eq!(cpu.bus.cpu_cycles, 4);
+            assert_eq!(cpu.pc, 2);
+        });
+
+        // Absolute mode
+        execute(&[0xad, 0x32, 0x01], |cpu| {
+            assert_eq!(cpu.a, 69);
+            assert_eq!(cpu.bus.cpu_cycles, 4);
+            assert_eq!(cpu.pc, 3);
+        });
+
+        // Absolute mode + x
+        execute(&[0xbd, 0x33, 0x00], |cpu| {
+            assert_eq!(cpu.a, 69);
+            assert_eq!(cpu.bus.cpu_cycles, 5);
+            assert_eq!(cpu.pc, 3);
+        });
+    }
+
+    #[test]
+    fn ldx() {
+        execute(&[0xa2, 69], |cpu| assert_eq!(cpu.x, 69));
     }
 }
